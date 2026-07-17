6d957d46820d86c4ca27d4f7e0145721c560b966
Mexii <142121295+cpMexii@users.noreply.github.com>
2026-07-17 09:57:11 -0500

    Add connected recurring payments Dashboard box

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "truebalance-budget-v7";
+const CACHE = "truebalance-budget-v8";
 const FILES = ["./", "./index.html", "./styles.css", "./config.js", "./supabase-client.js", "./app.js", "./favicon.svg", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
